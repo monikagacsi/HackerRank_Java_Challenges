e8990065ea752a309bfe8fe65475a2a077918e8f
monikagacsi <76663426+monikagacsi@users.noreply.github.com>
2022-09-16 15:26:34 +0200

    Java Substring Comparison

---
+++ src/com/hackerrank/JavaSubstringComparisons.java
@@ -0,0 +1,24 @@
+package com.hackerrank;
+
+import java.util.Scanner;
+
+public class JavaSubstringComparisons {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        int k = sc.nextInt();
+        String minSubstring = line.substring(0,k);
+        String maxSubstring = line.substring(0,k);
+        for (int i = 1; i < line.length()-k+1; i++) {
+            String sub = line.substring(i,i+k);
+            if (sub.compareTo(minSubstring) < 0) {
+                minSubstring = sub;
+            }
+            if (sub.compareTo(maxSubstring) > 0) {
+                maxSubstring = sub;
+            }
+        }
+        System.out.println(minSubstring);
+        System.out.println(maxSubstring);
+    }
+}
